ff29c2971537be97746a11cc8f211bf2f5697009
YashG92 <ygworld76@gmail.com>
2025-03-07 22:03:04 +0530

    seperate the weekly sales widget and added admin id and pass in login controller oninit

--- lib/features/shop/screens/dashboard/responsive_screens/dashboard_desktop.dart
+++ lib/features/shop/screens/dashboard/responsive_screens/dashboard_desktop.dart
@@ -1,13 +1,7 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart%20%20';
 import 'package:yt_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
-import 'package:yt_ecommerce_admin_panel/utils/constants/colors.dart';
+import 'package:yt_ecommerce_admin_panel/features/shop/screens/dashboard/widgets/weekly_sales.dart';
 import 'package:yt_ecommerce_admin_panel/utils/constants/sizes.dart';
-import 'package:yt_ecommerce_admin_panel/utils/device/device_utility.dart';
-import 'package:yt_ecommerce_admin_panel/utils/helpers/helper_functions.dart';
-
-import '../../../controllers/dashboard_controller.dart';
 import '../widgets/dashboard_card.dart';
 
 class DashboardDesktop extends StatelessWidget {
@@ -15,7 +9,6 @@ class DashboardDesktop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(DashboardController());
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
@@ -79,65 +72,7 @@ class DashboardDesktop extends StatelessWidget {
                     child: Column(
                       children: [
                         ///Bar Graph
-                        TRoundedContainer(
-                          child: Column(
-                            children: [
-                              Text(
-                                'Weekly Sales',
-                                style:
-                                    Theme.of(context).textTheme.headlineSmall,
-                              ),
-                              SizedBox(
-                                height: TSizes.spaceBtwItems,
-                              ),
-
-                              //Graph
-                              SizedBox(
-                                height: 400,
-                                child: BarChart(BarChartData(
-                                    titlesData: buildFlTitlesData(),
-                                    borderData: FlBorderData(
-                                        show: true,
-                                        border: Border(
-                                            top: BorderSide.none,
-                                            right: BorderSide.none)),
-                                    gridData: FlGridData(
-                                      show: true,
-                                      drawVerticalLine: false,
-                                      drawHorizontalLine: true,
-                                      horizontalInterval: 200,
-                                    ),
-                                    barGroups: controller.weeklySales
-                                        .asMap()
-                                        .entries
-                                        .map((entry) => BarChartGroupData(
-                                                x: entry.key,
-                                                barRods: [
-                                                  BarChartRodData(
-                                                    width: 30,
-                                                    color: TColors.primary,
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            TSizes.sm),
-                                                    toY: entry.value,
-                                                  )
-                                                ]))
-                                        .toList(),
-                                    groupsSpace: TSizes.spaceBtwItems,
-                                    barTouchData: BarTouchData(
-                                        touchTooltipData: BarTouchTooltipData(
-                                            getTooltipColor: (_) =>
-                                                TColors.secondary),
-                                        touchCallback:
-                                            TDeviceUtils.isDesktopScreen(
-                                                    context)
-                                                ? (barTouchEvent,
-                                                    barTouchResponse) {}
-                                                : null))),
-                              ),
-                            ],
-                          ),
-                        ),
+                        WeeklySalesGraph(),
                         SizedBox(
                           height: TSizes.spaceBtwSections,
                         ),
@@ -163,40 +98,6 @@ class DashboardDesktop extends StatelessWidget {
     );
   }
 
-  FlTitlesData buildFlTitlesData() {
-    return FlTitlesData(
-      show: true,
-      bottomTitles: AxisTitles(
-        sideTitles: SideTitles(
-            showTitles: true,
-            getTitlesWidget: (value, meta) {
-              //Map index to the desired day of week
-              final days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-              //Calculate the index and ensure it wraps around for the correct day
-              final index = value.toInt() % days.length;
-
-              //Get the day corresponding to the calculated index
-              final day = days[index];
-
-              return SideTitleWidget(
-                meta: meta,
-                space: 0,
-                child: Text(day),
-              ); //axisSide bottom depreciated
-            }),
-      ),
-      leftTitles: AxisTitles(
-        sideTitles: SideTitles(
-          showTitles: true,
-          interval: 200,
-          reservedSize: 50,
-        ),
-      ),
-      rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-      topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-    );
-  }
 }
 
 // class MyData extends DataTableSource {
