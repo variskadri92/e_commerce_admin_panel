98eedf67f61ce19ce6f927bccfd027ddaadf812f
variskadri92 <dzire192@gmail.com>
2025-03-11 16:01:56 +0530

    function in media repo to fetch images based on category

--- lib/data/repositories/media/media_repository.dart
+++ lib/data/repositories/media/media_repository.dart
@@ -7,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import '../../../features/media/models/image_model.dart';
+import '../../../utils/constants/enums.dart';
 
 class MediaRepository extends GetxController {
   static MediaRepository get instance => Get.find();
@@ -20,16 +21,16 @@ class MediaRepository extends GetxController {
       required String mimeType,
       required String path,
       required String imageName}) async {
-
-    try{
+    try {
       //Reference to the storage location
       final Reference ref = _storage.ref('$path/$imageName');
 
       //Upload the file using uint8list
-      final UploadTask uploadTask = ref.putData(fileData, SettableMetadata(contentType: mimeType));
+      final UploadTask uploadTask =
+          ref.putData(fileData, SettableMetadata(contentType: mimeType));
 
       //Wait for the upload to complete
-      final TaskSnapshot snapshot = await uploadTask.whenComplete(()=>{});
+      final TaskSnapshot snapshot = await uploadTask.whenComplete(() => {});
 
       //Get the download url
       final String downloadUrl = await snapshot.ref.getDownloadURL();
@@ -37,30 +38,55 @@ class MediaRepository extends GetxController {
       //Fetch Metadata
       final metadata = await ref.getMetadata();
 
-      return ImageModel.fromFirebaseMetadata(metadata, path, imageName, downloadUrl);
-    }on FirebaseException catch (e) {
+      return ImageModel.fromFirebaseMetadata(
+          metadata, path, imageName, downloadUrl);
+    } on FirebaseException catch (e) {
       throw e.message!;
     } on SocketException catch (e) {
       throw e.message;
     } on PlatformException catch (e) {
       throw e.message!;
-    }catch (e) {
+    } catch (e) {
       throw e.toString();
     }
   }
-  
+
   ///Upload image data in firstore
   Future<String> uploadImageFileInDatabase(ImageModel image) async {
-    try{
-      final data = await FirebaseFirestore.instance.collection("Images").add(image.toJson());
+    try {
+      final data = await FirebaseFirestore.instance
+          .collection("Images")
+          .add(image.toJson());
       return data.id;
-    }on FirebaseException catch (e) {
+    } on FirebaseException catch (e) {
       throw e.message!;
     } on SocketException catch (e) {
       throw e.message;
     } on PlatformException catch (e) {
       throw e.message!;
-    }catch (e) {
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+  ///Fetch images from Firestore based on media category and load count
+  Future<List<ImageModel>> fetchImagesFromDatabase(
+      MediaCategory mediaCategory, int loadCount) async {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection("Images")
+          .where("mediaCategory", isEqualTo: mediaCategory.name.toString())
+          .orderBy("createdAt", descending: true)
+          .limit(loadCount)
+          .get();
+      return querySnapshot.docs.map((e) => ImageModel.fromSnapshot(e)).toList();
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } on SocketException catch (e) {
+      throw e.message;
+    } on PlatformException catch (e) {
+      throw e.message!;
+    } catch (e) {
       throw e.toString();
     }
   }
