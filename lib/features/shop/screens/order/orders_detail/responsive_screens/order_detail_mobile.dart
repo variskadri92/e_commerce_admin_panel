049ab0a1bb0ea99111919058540cf7cafc5b2fb0
YashG92 <ygworld76@gmail.com>
2025-03-14 16:34:19 +0530

    order customer mobile screen adjustments

--- lib/features/shop/screens/order/orders_detail/responsive_screens/order_detail_mobile.dart
+++ lib/features/shop/screens/order/orders_detail/responsive_screens/order_detail_mobile.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
+import '../../../../../../routes/routes.dart';
+import '../../../../../../utils/constants/sizes.dart';
+import '../widgets/order_customer.dart';
+import '../widgets/order_info.dart';
+import '../widgets/order_items.dart';
+import '../widgets/order_transaction.dart';
 import '../../../../models/order_model.dart';
 
 class OrderDetailMobile extends StatelessWidget {
@@ -10,6 +17,51 @@ class OrderDetailMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Breadcrumbs
+                BreadcrumbWithHeading(
+                    returnToPreviousScreen: true,
+                    heading: order.id,
+                    breadcrumbItems: [Routes.orders, 'Details']),
+                const SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                // Body
+                OrderInfo(
+                  order: order,
+                ),
+                const SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                //Items
+                OrderItems(order :order),
+                const SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                //Transaction
+                OrderTransaction(order: order),
+
+                SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                //Right Side Customer Order History
+                OrderCustomer(
+                  order: order,
+                ),
+                SizedBox(height: TSizes.spaceBtwSections,)
+              ],
+            ),
+          ),
+        ));
   }
 }
