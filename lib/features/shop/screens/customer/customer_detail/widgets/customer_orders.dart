f020619ca980bf4751dade9a0d864fa09dc3cac2
YashG92 <ygworld76@gmail.com>
2025-03-14 10:59:55 +0530

    customer order card design

--- lib/features/shop/screens/customer/customer_detail/widgets/customer_orders.dart
+++ lib/features/shop/screens/customer/customer_detail/widgets/customer_orders.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:yt_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:yt_ecommerce_admin_panel/utils/constants/colors.dart';
+import 'package:yt_ecommerce_admin_panel/utils/constants/sizes.dart';
 
 class CustomerOrders extends StatelessWidget {
   const CustomerOrders({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TRoundedContainer(
+      padding: EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Orders',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(text: 'Total Spent '),
+                    TextSpan(text: '\$1234.65',style: Theme.of(context).textTheme.bodyLarge!.apply(color: TColors.primary)),
+                    TextSpan(text: ' on ${4} Orders'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: TSizes.spaceBtwItems),
+          TextFormField(
+            onChanged: (query){},
+            decoration: InputDecoration(
+              hintText: 'Search Orders',
+              prefixIcon: Icon(Iconsax.search_normal),
+            ),
+          ),
+
+          SizedBox(height: TSizes.spaceBtwSections,),
+          CustomerOrderTable(),
+        ],
+      ),
+    );
   }
 }
