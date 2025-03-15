aaa0a55fa2c951f02a0a0872705f5216dca2f639
YashG92 <ygworld76@gmail.com>
2025-03-15 16:09:05 +0530

    create and update category controller created

---
+++ lib/features/shop/controllers/category/create_category_controller.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:yt_ecommerce_admin_panel/features/shop/models/category_model.dart';
+
+class CreateCategoryController extends GetxController{
+  static CreateCategoryController get instance => Get.find();
+
+  final selectedParent = CategoryModel.empty().obs;
+  final loading = false.obs;
+  RxString imageURL = ''.obs;
+  final isFeatured = false.obs;
+  final name = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  ///Init Data
+  ///Method to reset fields
+  ///Pick Thumbnail Image from Media
+  ///Register new category
+  Future<void> createCategory(CategoryModel category) async {}
+}
