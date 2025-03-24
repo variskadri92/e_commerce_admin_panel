1e02cebff1cb6a6f44b48195a8d187cf2cfb88ab
YashG92 <ygworld76@gmail.com>
2025-03-24 13:12:55 +0530

    order controller

---
+++ lib/features/shop/controllers/order/order_controller.dart
@@ -0,0 +1,59 @@
+import 'package:get/get.dart';
+import 'package:yt_ecommerce_admin_panel/data/abstract/base_data_table_controller.dart';
+import 'package:yt_ecommerce_admin_panel/data/repositories/order/order_repository.dart';
+import 'package:yt_ecommerce_admin_panel/features/shop/models/order_model.dart';
+import 'package:yt_ecommerce_admin_panel/utils/constants/enums.dart';
+import 'package:yt_ecommerce_admin_panel/utils/popups/loaders.dart';
+
+class OrderController extends BaseDataTableController<OrderModel> {
+  static OrderController get instance => Get.find();
+
+  RxBool statusLoader = false.obs;
+  var orderStatus = OrderStatus.delivered.obs;
+  final _orderRepository = Get.put(OrderRepository());
+
+  @override
+  Future<List<OrderModel>> fetchItems() async {
+    sortAscending.value = false;
+    return await _orderRepository.getAllOrders();
+  }
+
+  @override
+  bool containsSearchQuery(OrderModel item, String query) {
+    return item.id.toLowerCase().contains(query.toLowerCase());
+  }
+
+  @override
+  Future<void> deleteItems(OrderModel item) async {
+    await _orderRepository.deleteOrder(item.docId);
+  }
+
+  void sortById(int sortColumnIndex, bool ascending) {
+    sortByProperty(sortColumnIndex, ascending,
+        (OrderModel o) => o.totalAmount.toString().toLowerCase());
+  }
+
+  void sortByDate(int sortColumnIndex, bool ascending) {
+    sortByProperty(sortColumnIndex, ascending,
+        (OrderModel o) => o.orderDate.toString().toLowerCase());
+  }
+
+  ///Update Product Status
+  Future<void> updateOrderStatus(
+      OrderModel order, OrderStatus newStatus) async {
+    try {
+      statusLoader.value = true;
+      order.status = newStatus;
+      await _orderRepository.updateOrderSpecificValue(
+          order.docId, {'status': newStatus.toString()});
+      updateItemFromLists(order);
+      orderStatus.value = newStatus;
+      TLoaders.successSnackBar(
+          title: 'Updated', message: 'Order Status Updated');
+    } catch (e) {
+      TLoaders.warningSnackBar(title: 'Oh Snap!', message: e.toString());
+    } finally {
+      statusLoader.value = false;
+    }
+  }
+}
