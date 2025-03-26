5993896843cb45d3c413d3a479f93b278b8ed14b
YashG92 <ygworld76@gmail.com>
2025-03-26 13:21:41 +0530

    dashboard order table source

--- lib/features/shop/screens/dashboard/table/table_source.dart
+++ lib/features/shop/screens/dashboard/table/table_source.dart
@@ -2,6 +2,8 @@ import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:yt_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:yt_ecommerce_admin_panel/features/shop/controllers/order/order_controller.dart';
+import 'package:yt_ecommerce_admin_panel/routes/routes.dart';
 import 'package:yt_ecommerce_admin_panel/utils/constants/colors.dart';
 import 'package:yt_ecommerce_admin_panel/utils/constants/sizes.dart';
 import 'package:yt_ecommerce_admin_panel/utils/helpers/helper_functions.dart';
@@ -9,10 +11,15 @@ import 'package:yt_ecommerce_admin_panel/utils/helpers/helper_functions.dart';
 import '../../../controllers/dashboard/dashboard_controller.dart';
 
 class OrderRows extends DataTableSource {
+  final controller = OrderController.instance;
   @override
   DataRow? getRow(int index) {
-    final order = DashboardController.orders[index];
-    return DataRow2(cells: [
+    final order = controller.filteredItems[index];
+    return DataRow2(
+        onTap: ()=> Get.toNamed(Routes.orderDetail,arguments: order),
+        selected: controller.selectedRows[index],
+        onSelectChanged: (value) =>controller.selectedRows[index] = value ?? false,
+        cells: [
       DataCell(
         Text(
           order.id,
@@ -43,8 +50,8 @@ class OrderRows extends DataTableSource {
   bool get isRowCountApproximate => false;
 
   @override
-  int get rowCount =>  DashboardController.orders.length;
+  int get rowCount => controller.filteredItems.length;
 
   @override
-  int get selectedRowCount => 0;
+  int get selectedRowCount => controller.selectedRows.where((element) => element).length;
 }
