d9883d20778426b4d4b86c5f50ac38d4e2d40e0f
YashG92 <ygworld76@gmail.com>
2025-03-26 13:18:48 +0530

    dashboard order table

--- lib/features/shop/screens/dashboard/table/dashboard_order_table.dart
+++ lib/features/shop/screens/dashboard/table/dashboard_order_table.dart
@@ -1,25 +1,38 @@
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart%20%20';
 import 'package:yt_ecommerce_admin_panel/common/widgets/data_table/paginated_data_table.dart';
+import 'package:yt_ecommerce_admin_panel/features/shop/controllers/order/order_controller.dart';
 import 'package:yt_ecommerce_admin_panel/features/shop/screens/dashboard/table/table_source.dart';
 import 'package:yt_ecommerce_admin_panel/utils/constants/sizes.dart';
+import 'package:yt_ecommerce_admin_panel/utils/device/device_utility.dart';
 
 class DashboardOrderTable extends StatelessWidget {
   const DashboardOrderTable({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TPaginatedDataTable(
-      minWidth: 700,
-        tableHeight: 500,
-        dataRowHeight: TSizes.xl * 1.2,
+    final controller = OrderController.instance;
+    return Obx(
+      () {
+        Text(controller.filteredItems.length.toString());
+        Text(controller.selectedRows.length.toString());
 
-        columns: [
-      DataColumn2(label: Text('Order ID')),
-      DataColumn2(label: Text('Date')),
-      DataColumn2(label: Text('Items')),
-      DataColumn2(label: Text('Status')),
-      DataColumn2(label: Text('Amount')),
-    ], source: OrderRows());
+        return TPaginatedDataTable(
+            minWidth: 700,
+            tableHeight: 500,
+            dataRowHeight: TSizes.xl * 1.2,
+            sortAscending: controller.sortAscending.value,
+            sortColumnIndex: controller.sortColumnIndex.value,
+            columns: [
+              DataColumn2(label: Text('Order ID'),onSort: (columnIndex, ascending) => controller.sortById(columnIndex, ascending)),
+              DataColumn2(label: Text('Date')),
+              DataColumn2(label: Text('Items')),
+              DataColumn2(label: Text('Status'),fixedWidth: TDeviceUtils.isMobileScreen(context)? 120 : null),
+              DataColumn2(label: Text('Amount')),
+            ],
+            source: OrderRows());
+      },
+    );
   }
 }
